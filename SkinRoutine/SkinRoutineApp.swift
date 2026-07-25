236c8f4a8aba9991e907dda705fcad230e538085
Ramya Canessane <ramyacanessane@icloud.com>
2026-07-25 15:10:11 +0200

    Initial Commit

---
+++ SkinRoutine/SkinRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinRoutineApp.swift
+//  SkinRoutine
+//
+//  Created by Canessane Ramya on 25/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct SkinRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkinRoutineTests/SkinRoutineTests.swift
@@ -0,0 +1,19 @@
+//
+//  SkinRoutineTests.swift
+//  SkinRoutineTests
+//
+//  Created by Canessane Ramya on 25/07/2026.
+//
+
+import Testing
+@testable import SkinRoutine
+
+struct SkinRoutineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
